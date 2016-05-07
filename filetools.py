79ff0876a50c57bdd4343c1488dcfd45826310c1
Bongsoo Suh <bongsoo.suh@gmail.com>
2016-05-07 16:57:18 -0700

    updated get_files to have substr argument input

--- filetools.py
+++ filetools.py
@@ -8,9 +8,13 @@ Access files in a directory
 from os import listdir
 from os.path import isfile, join
 
-def get_files(dir_path):
+def get_files(dir_path, substr=None):
     '''
     return a list of files in a directory
     '''
-    return [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    if substr:
+        return [f for f in listdir(dir_path) if isfile(join(dir_path, f)) and substr in join(dir_path, f)]
+
+    else:
+        return [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
